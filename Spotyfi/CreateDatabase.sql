894bb12106af3e690136fe1171707b6f324ca109
Johan Borregaard Draaby <zbcjodra2@zbc.dk>
2019-06-23 13:48:55 +0200

    Commit

---
+++ Spotyfi/CreateDatabase.sql
@@ -0,0 +1,64 @@
+# Create Database SQL Script
+
+CREATE DATABASE IF NOT EXISTS `spotyfi`;
+
+DROP TABLE IF EXISTS album_artist;
+DROP TABLE IF EXISTS album_song;
+DROP TABLE IF EXISTS song_artist;
+DROP TABLE IF EXISTS artist;
+DROP TABLE IF EXISTS album;
+DROP TABLE IF EXISTS song;
+
+CREATE TABLE song
+(
+	id INT AUTO_INCREMENT,
+	`name` VARCHAR(64),
+	`path` VARCHAR(256),
+	`track_duration_seconds` INT,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE album
+(
+	`id` INT AUTO_INCREMENT,
+	`name` VARCHAR(64),
+	`description` VARCHAR(256),
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE artist
+(
+	`id` INT AUTO_INCREMENT,
+	`real_name` VARCHAR(256),
+	`artist_name` VARCHAR(256),
+	PRIMARY KEY (`id`)
+);
+
+CREATE TABLE song_artist
+(
+	`song_id` INT NOT NULL,
+	`artist_id` INT NOT NULL,
+	PRIMARY KEY(`song_id`, `artist_id`),
+	FOREIGN KEY (song_id) REFERENCES song(id),
+	FOREIGN KEY (artist_id) REFERENCES artist(id)
+);
+
+CREATE TABLE album_song
+(
+	`album_id` INT NOT NULL,
+	`song_id` INT NOT NULL,
+	PRIMARY KEY(`album_id`, `song_id`),
+	FOREIGN KEY (album_id) REFERENCES album(id),
+	FOREIGN KEY (song_id) REFERENCES song(id)
+);
+
+CREATE TABLE album_artist
+(
+	`album_id` INT NOT NULL,
+	`artist_id` INT NOT NULL,
+	PRIMARY KEY(`album_id`, `artist_id`),
+	FOREIGN KEY (album_id) REFERENCES album(id),
+	FOREIGN KEY (artist_id) REFERENCES artist(id)
+);
+
+#########################################################################################################################################
---
+++ Spotyfi/CreateDummyData.sql
@@ -0,0 +1,56 @@
+#############################SONGS########################################################################################
+
+# Dahler Mendi
+INSERT INTO song(`id`, `name`, `path`, `track_duration_seconds`) VALUES (1, 'Tunak Tun', 'C:\\music_db\\1.mp3', 303);
+# Eminem
+INSERT INTO song(`id`, `name`, `path`, `track_duration_seconds`) VALUES (2, 'Without Me', 'C:\\music_db\\2.mp3', 290);
+# Evanescense
+INSERT INTO song(`id`, `name`, `path`, `track_duration_seconds`) VALUES (3, 'Bring Me To Life', 'C:\\music_db\\3.mp3', 235);
+# Ace hood, Future, Rick Ross
+INSERT INTO song(`id`, `name`, `path`, `track_duration_seconds`) VALUES (4, 'Bugatti', 'C:\\music_db\\4.mp3', 269);
+# Kanye West
+INSERT INTO song(`id`, `name`, `path`, `track_duration_seconds`) VALUES (5, 'POWER', 'C:\\music_db\\5.mp3', 292);
+# Post Malone, Swae Lee
+INSERT INTO song(`id`, `name`, `path`, `track_duration_seconds`) VALUES (6, 'Sunflower - Spider-Man: Into the Spider-Verse', 'C:\\music_db\\6.mp3', 158);
+
+##########################ARTISTS##########################################################################################
+
+INSERT INTO artist(`id`, artist.real_name, artist.artist_name) VALUES (1, 'Dahler Mendi', 'Dahler Mendi');
+INSERT INTO artist(`id`, artist.real_name, artist.artist_name) VALUES (2, 'Marshall Bruce Mathers III', 'Eminem');
+INSERT INTO artist(`id`, artist.real_name, artist.artist_name) VALUES (3, 'Amy Lee', 'Evanescense');
+INSERT INTO artist(`id`, artist.real_name, artist.artist_name) VALUES (4, 'Antoine McColister', 'Ace Hood');
+INSERT INTO artist(`id`, artist.real_name, artist.artist_name) VALUES (5, 'Nayvadius DeMun Wilburn', 'Future');
+INSERT INTO artist(`id`, artist.real_name, artist.artist_name) VALUES (6, 'William Leonard Roberts II', 'Rick Ross');
+INSERT INTO artist(`id`, artist.real_name, artist.artist_name) VALUES (7, 'Kanye West', 'Kanye West');
+INSERT INTO artist(`id`, artist.real_name, artist.artist_name) VALUES (8, 'Austin Richard Post', 'Post Malone');
+INSERT INTO artist(`id`, artist.real_name, artist.artist_name) VALUES (9, 'Khalif Malik Ibn Shaman Brown', 'Swae Lee');
+
+##########################ALBUMS###########################################################################################
+
+INSERT INTO album(album.id, album.name, album.description) VALUES(1, 'Tunak Tunak Tun', 'N/A');
+INSERT INTO album(album.id, album.name, album.description) VALUES(2, 'The Eminem Show', 'N/A');
+INSERT INTO album(album.id, album.name, album.description) VALUES(3, 'Fallen', 'N/A');
+INSERT INTO album(album.id, album.name, album.description) VALUES(4, 'Trials and Tribulations', 'N/A');
+INSERT INTO album(album.id, album.name, album.description) VALUES(5, 'My Beautiful Dark Twisted Fantasy', 'N/A');
+INSERT INTO album(album.id, album.name, album.description) VALUES(6, 'Spider-Man: Into the Spider-Verse', 'N/A');
+
+##########################album_song########################################################################################
+
+INSERT INTO album_song(album_song.album_id, album_song.song_id) VALUES (1, 1);
+INSERT INTO album_song(album_song.album_id, album_song.song_id) VALUES (2, 2);
+INSERT INTO album_song(album_song.album_id, album_song.song_id) VALUES (3, 3);
+INSERT INTO album_song(album_song.album_id, album_song.song_id) VALUES (4, 4);
+INSERT INTO album_song(album_song.album_id, album_song.song_id) VALUES (5, 5);
+INSERT INTO album_song(album_song.album_id, album_song.song_id) VALUES (6, 6);
+
+###########################album_artist######################################################################################
+
+INSERT INTO album_artist(album_artist.album_id, album_artist.artist_id) VALUES (1,1);
+INSERT INTO album_artist(album_artist.album_id, album_artist.artist_id) VALUES (2,2);
+INSERT INTO album_artist(album_artist.album_id, album_artist.artist_id) VALUES (3,3);
+INSERT INTO album_artist(album_artist.album_id, album_artist.artist_id) VALUES (4,4);
+INSERT INTO album_artist(album_artist.album_id, album_artist.artist_id) VALUES (4,5);
+INSERT INTO album_artist(album_artist.album_id, album_artist.artist_id) VALUES (4,6);
+INSERT INTO album_artist(album_artist.album_id, album_artist.artist_id) VALUES (5,7);
+INSERT INTO album_artist(album_artist.album_id, album_artist.artist_id) VALUES (6,8);
+INSERT INTO album_artist(album_artist.album_id, album_artist.artist_id) VALUES (6,9);
